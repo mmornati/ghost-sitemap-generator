9e102d77751fae62da734d176a6b612cc5df79c8
Marco Mornati <ilmorna@gmail.com>
2014-02-10 23:48:30 +0100

    First commit. Script working needs improvements to support any possible Ghost configuration

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ ghost-sitemap-generator
 =======================
 
 Sitemap Generator for Ghost blogging reading posts directly from database
+
+INFO:
+The script is actually working and it produces a working (and complete)
+sitemap.xml file. 
+I need to add checks and some improvements to consider this script dynamics and
+usable everywhere.
+
+
+Crontab Example
+---------------
+
+0 0 * * * /usr/bin/ruby generate_ghost_sitemap.rb blog.mornati.net daily 0.5 /path/to/ghost/sitemap.xml
+
+
---
+++ generate_ghost_sitemap.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'builder'
+require 'mysql'
+require 'time'
+require 'builder/xmlmarkup'
+require 'open-uri'
+
+class MyXmlMarkup < ::Builder::XmlMarkup
+  def tag!(sym, *args, &block)
+    if @level == 0 # Root element
+      args << {"xmlns" => "http://www.sitemaps.org/schemas/sitemap/0.9",
+               "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
+               "xsi:schemaLocation" => "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"}
+    end
+
+    #if args[:nil] = true
+    #  args << {"xsi:nil" => true}
+    #end
+
+    super(sym, *args, &block)
+  end
+end
+
+
+begin
+    site_base_url = ARGV[0]
+    change_freq = ARGV[1]
+    priority = ARGV[2]
+    dest_file = ARGV[3]
+    con = Mysql.new 'localhost', 'ghost', 'ghostPWD', 'ghost'
+    rs = con.query "select slug,updated_at from posts where status='published' order by id desc;"
+    puts "Number of posts #{rs.num_rows}"
+    #xml = Builder::XmlMarkup.new( :indent => 2 )
+    xml = MyXmlMarkup.new( :indent => 2 )
+    xml.instruct! :xml, :encoding => "ASCII"
+    xml.urlset do |urlset|
+      rs.each_hash do |post|
+        urlset.url do |p|
+           #TODO: read settings -> permalinks
+           #      read blog name
+           p.log 'http://' + site_base_url + '/' + post['slug'] + '/'
+           p.lastmod Time.parse(post['updated_at']).strftime("%Y-%m-%dT%H:%M:%S+00:00")
+           p.changefreq change_freq
+           p.priority priority
+        end
+      end
+    end
+    xml_data = xml.target!
+    file = File.new(dest_file, "wb")
+    file.write(xml_data)
+    file.close
+ 
+    #Calling Google
+    url_google = "http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2F#{site_base_url}%2Fsitemap.xml"
+    response = open(url_google).read
+    puts response    
+rescue Mysql::Error => e
+    puts e.errno
+    puts e.error
+    
+ensure
+    con.close if con
+end
