2cb8a7f4903dd98d3f7a96a8c063ea39561f82a9
Marco Mornati <ilmorna@gmail.com>
2014-02-11 22:31:11 +0100

    Added OptParse for Script Parameters Generated Pages Link

--- generate_ghost_sitemap.rb
+++ generate_ghost_sitemap.rb
@@ -6,6 +6,8 @@ require 'mysql'
 require 'time'
 require 'builder/xmlmarkup'
 require 'open-uri'
+require 'optparse'
+require 'pp'
 
 class MyXmlMarkup < ::Builder::XmlMarkup
   def tag!(sym, *args, &block)
@@ -25,42 +27,117 @@ end
 
 
 begin
-    site_base_url = ARGV[0]
-    change_freq = ARGV[1]
-    priority = ARGV[2]
-    dest_file = ARGV[3]
-    con = Mysql.new 'localhost', 'ghost', 'ghostPWD', 'ghost'
+    options = {}
+
+    optparse = OptionParser.new do |opts|
+        opts.banner = "Usage: generate_ghost_sitemap.rb [options]"
+        opts.on( '-h', '--help', 'Display this screen' ) do
+           puts opts
+           exit
+        end
+        opts.on( '-s', '--site SITE', 'Site base URL. EX: blog.mornati.net' ) do |site|
+           options[:site] = site
+        end
+        opts.on( '-f', '--frequency FREQUENCY', 'Update Frenquency. One of: always,hourly,daily,weekly,monthly,yearly,never' ) do |freq|
+           options[:frequency] = freq
+        end
+        opts.on( '-p', '--priority PRIORITY', 'Update priority. Values beetwen 0.0 et 1.0' ) do |priority|
+           options[:priority] = priority
+        end
+        opts.on( '-d', '--destfile DESTFILE', 'Sitemap destination file. Ex. /usr/share/server/sitemap.xml' ) do |dfile|
+           options[:destfile] = dfile
+        end
+        opts.on( '-t', '--test', 'Do not ping Google after sitemap generation' ) do |v|
+           options[:test] = v
+        end
+        opts.on( '-v', '--verbose', 'Verbose execution' ) do |v|
+           options[:verbose] = v
+        end
+        opts.on( '-m', '--mysql HOSTNAME', 'MySQL hostname' ) do |host|
+           options[:hostname] = host
+        end
+        opts.on( '-u', '--user USERNAME', 'MySQL Username' ) do |user|
+           options[:user] = user
+        end
+        opts.on( '-w', '--password PASSWORD', 'MySQL Password' ) do |password|
+           options[:password] = password
+        end
+        opts.on( '-b', '--dbname DBNAME', 'Database name' ) do |db|
+           options[:dbname] = db
+        end
+    end
+
+    begin
+        optparse.parse!
+        mandatory = [:site, :priority, :frequency, :destfile, :hostname, :user, :password, :dbname]
+        missing = mandatory.select{ |param| options[param].nil? }        
+        if not missing.empty?                                            
+          puts "Missing options: #{missing.join(', ')}"                  
+          puts optparse                                                  
+          exit                                                           
+        end                                                              
+    rescue OptionParser::InvalidOption, OptionParser::MissingArgument      
+        puts $!.to_s                                                           
+        puts optparse                                                          
+        exit                                                                   
+    end 
+
+    puts "Options Values:" unless !options[:verbose]
+    pp options unless !options[:verbose] 
+    
+    con = Mysql.new options[:hostname], options[:user], options[:password], options[:dbname]
     rs = con.query "select slug,updated_at from posts where status='published' order by id desc;"
-    puts "Number of posts #{rs.num_rows}"
+    tot_posts = rs.num_rows
+    puts "Number of posts #{tot_posts}"
     xml = MyXmlMarkup.new( :indent => 2 )
     xml.instruct! :xml, :encoding => "ASCII"
     xml.urlset do |urlset|
         urlset.url do |baseurl|
-            baseurl.loc 'http://' + site_base_url
+            baseurl.loc 'http://' + options[:site]
             baseurl.lastmod Time.now.strftime("%Y-%m-%dT%H:%M:%S+00:00")
-            baseurl.changefreq change_freq
-            baseurl.priority priority
+            baseurl.changefreq options[:frequency]
+            baseurl.priority options[:priority]
         end
         rs.each_hash do |post|
           urlset.url do |p|
              #TODO: read settings -> permalinks
              #      read blog name
-             p.loc 'http://' + site_base_url + '/' + post['slug'] + '/'
+             p.loc 'http://' + options[:site] + '/' + post['slug'] + '/'
              p.lastmod Time.parse(post['updated_at']).strftime("%Y-%m-%dT%H:%M:%S+00:00")
-             p.changefreq change_freq
-             p.priority priority
+             p.changefreq options[:frequency]
+             p.priority options[:priority]
           end
+        end
+        rs = con.query "select value from settings as s where s.key = 'postsPerPage';"
+        postsPerPage=0
+        rs.each_hash do |p|
+	    postsPerPage = p['value'].to_i
+        end
+        
+        puts "PostsPerPage: #{postsPerPage}" unless !options[:verbose] 
+	totalPages = (tot_posts.to_f / postsPerPage.to_f).round
+        puts "Total Pages: #{totalPages}" unless !options[:verbose]
+	puts "Generating Pages URL..." unless !options[:verbose]
+        for n in 1 ... totalPages+1
+            urlset.url do |p|
+               p.loc 'http://' + options[:site] + "/page/#{n}/"
+               p.lastmod Time.now.strftime("%Y-%m-%dT%H:%M:%S+00:00")
+               p.changefreq options[:frequency]
+               p.priority options[:priority]
+            end
         end
     end
     xml_data = xml.target!
-    file = File.new(dest_file, "wb")
+    file = File.new(options[:destfile], "wb")
     file.write(xml_data)
     file.close
  
     #Calling Google
-    url_google = "http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2F#{site_base_url}%2Fsitemap.xml"
-    response = open(url_google).read
-    puts response    
+    unless options[:test]
+    	url_google = "http://www.google.com/webmasters/tools/ping?sitemap=http%3A%2F%2F#{options[:site]}%2Fsitemap.xml"
+    	response = open(url_google).read
+    	puts response unless !options[:verbose] 
+    end
 rescue Mysql::Error => e
     puts e.errno
     puts e.error
